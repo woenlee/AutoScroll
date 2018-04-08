0ef8eb18ce01a964029839b5e01ffbc6a7045811
Woen Lee <woen.lee1@gmail.com>
2018-04-07 23:45:22 -0700

    added options

--- popup.js
+++ popup.js
@@ -1,13 +1,14 @@
 let changeColor = document.getElementById('changeColor');
 
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-};
-
 chrome.storage.sync.get('color', function(data) {
     changeColor.style.backgroundColor = data.color;
     changeColor.setAttribute('value', data.color);
 });
+
+changeColor.onclick = function(element) {
+    let color = element.target.value;
+    console.log('color is ' + color);
+    chrome.tabs.executeScript(
+        //tabs[0].id,
+        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+};
