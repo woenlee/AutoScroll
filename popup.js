adcf057f6237791fbc36377ac54228a049f7cbdc
Woen Lee <woen.lee1@gmail.com>
2018-04-07 22:14:37 -0700

    finished

---
+++ popup.js
@@ -0,0 +1,13 @@
+let changeColor = document.getElementById('changeColor');
+
+changeColor.onclick = function(element) {
+    let color = element.target.value;
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+};
+
+chrome.storage.sync.get('color', function(data) {
+    changeColor.style.backgroundColor = data.color;
+    changeColor.setAttribute('value', data.color);
+});
