e5d4f83a1481e10c8bd270fdb608aaf6b965bb6d
Woen Lee <woen.lee1@gmail.com>
2018-04-10 12:49:03 -0700

    skeletal functionality

--- popup.js
+++ popup.js
@@ -1,13 +1,31 @@
 let startScroll = document.getElementById('startScroll');
 startScroll.style.backgroundColor = '#00FF7F';
-
-//Scroll window object
+var pixels,waitTime;
+//Scroll  object
 startScroll.onclick = function begin() {
-    setInterval(scroll, 100);
+    var slider = document.getElementById("myRange");
+    if(slider.value < 300)
+        waitTime = 300 - slider.value;
+    else
+        waitTime = 1;
+    pixels = 1;
+    if(slider.value >= 300)
+        pixels = 2;
+    else if(slider.value >= 350)
+        pixels = 3;
+    else if(slider.value >= 400)
+        pixels = 4;
+    else if(slider.value >= 450)
+        pixels = 5;
+    else if(slider.value >= 500)
+        pixels = 6;
+    setInterval(scroll, waitTime);
 }
 
 function scroll(element) {
-    console.log('scrolling!');
     chrome.tabs.executeScript(
-        {code: 'window.scrollBy(0,2);'});
+        {
+            code: 'window.scrollBy(0, ' + pixels + ');'
+        }
+    );
 };
