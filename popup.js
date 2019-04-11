cebf3d11f2c9cc707c3b3318b3cf93f75a32caf8
Jun Lee <jy45lee@gmail.com>
2019-04-11 10:02:14 -0700

    Stop button and slider stuff (#1)
    * added stop button
    
    * change scroll speed when changing slider
    
    * moved script to its own function
    
    * starting local storage

--- popup.js
+++ popup.js
@@ -1,33 +1,79 @@
-let startScroll = document.getElementById('startScroll');
-startScroll.style.backgroundColor = '#00FF7F';
-var pixels,waitTime,timeout;
+var pixels,waitTime,timeout, sliderVal;
+var startScroll = document.getElementById('startScroll');
+var stopScroll  = document.getElementById('stopScroll');
+var slider      = document.getElementById("myRange");
+
+startScroll.style.backgroundColor   = '#00FF7F';
+stopScroll.style.backgroundColor    = '#FF007F';
 
 //Scroll object
-startScroll.onclick = function begin() {
+startScroll.onclick = setSlider;
+slider.onchange     = setSlider;
+
+stopScroll.onclick = function(){
+    pixels = 0;
+};
+
+document.onreadystatechange = function() {
+    console.log("local storage");
+    var saved_timeout;
+    var saved_pixel;
+    var saved_slider;
+
+    saved_timeout   = localStorage.getItem("timeout");
+    saved_pixel     = localStorage.getItem("pixel");
+    saved_slider    = localStorage.getItem("slider");
+
+    if(saved_timeout)   timeout     = saved_timeout;
+    if(saved_pixel)     pixels      = saved_pixel;
+    if(saved_slider)    sliderVal   = saved_slider;
+};
+
+function setSlider(){
     clearTimeout(timeout);
-    var slider = document.getElementById("myRange");
-    if(slider.value < 300)
+    if (slider.value < 300)
         waitTime = 300 - slider.value;
     else
         waitTime = 1;
+
     pixels = 1;
-    if(slider.value >= 300)
+    if (slider.value >= 300)
         pixels = 2;
-    else if(slider.value >= 350)
+    else if (slider.value >= 350)
         pixels = 3;
-    else if(slider.value >= 400)
+    else if (slider.value >= 400)
         pixels = 4;
-    else if(slider.value >= 450)
+    else if (slider.value >= 450)
         pixels = 5;
-    else if(slider.value >= 500)
+    else if (slider.value >= 500)
         pixels = 6;
+
     timeout = setInterval(scroll, waitTime);
+
+    localStorage.setItem("pixels",  JSON.stringify(pixels));
+    localStorage.setItem("timeout", JSON.stringify(timeout));
+    localStorage.setItem("slider",  JSON.stringify(slider.value));
 }
 
 function scroll(element) {
-    chrome.tabs.executeScript(
-        {
-            code: 'window.scrollBy(0, ' + pixels + ');'
-        }
-    );
-};
+    chrome.tabs.executeScript({
+        code: returnScript()
+    });
+}
+
+function returnScript(){
+    var script = [
+        'if(document.title.includes(\'Google Sheets\')){',
+
+        '}else if(document.title.includes(\'Google Doc\')){',
+
+        '}else{',
+            'window.scrollBy(0, ', pixels, ');',
+        '}'
+    ];
+
+    var joinedScript = script.join('');
+    console.log(joinedScript);
+
+    return joinedScript;
+}
